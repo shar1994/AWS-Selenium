921ef03e4e85fc95774faa1344ae33799b035886
shar1994 <53858653+shar1994@users.noreply.github.com>
2020-12-06 15:45:57 +0530

    12/6/2020

--- com.automation.aws.core/src/main/java/com/automation/aws/driver/Driver.java
+++ com.automation.aws.core/src/main/java/com/automation/aws/driver/Driver.java
@@ -2,6 +2,7 @@ package com.automation.aws.driver;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -58,7 +59,7 @@ public class Driver {
 		System.out.println("Navigated to :- " + baseUrl);
 		// Maximize the browser
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(baseUrl + "");
 
 	}
@@ -81,7 +82,9 @@ public class Driver {
 //	}
 
 //	report.flush();
+		driver.close();
 		driver.quit();
+		
 
 	}
 
